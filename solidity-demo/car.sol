06b3be25a975d0a5ccdd224119f238d1b221db22
zhuzhenfeng <zhuzhenfeng@finogeeks.com>
2018-08-18 17:17:37 +0800

    add car

---
+++ solidity-demo/car.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.20;
+
+contract Car {
+  uint speed;
+  string name;
+
+  function Car() public {
+    speed = 100;
+    name="奔驰";
+  }
+
+  function getSpeed() constant public returns (uint) {
+    return speed;
+  }
+
+  function setSpeed(uint x) public {
+    speed=x;
+  }
+
+  function currentAddr() view public returns (address) {
+    return msg.sender;
+  }
+
+}
